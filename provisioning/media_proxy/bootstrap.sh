d636b9c7f8ec59b2e08b30ff40edbb8bf4f8a282
BallMan <francesco.mistri98@gmail.com>
2021-01-05 13:40:45 +0100

    Added second machine: media_proxy
    The media proxy bootstrap requires an argument(can be inserted in the config.json), its IP address, since it has to be specified for RTPProxy to work properly.
    
    The media proxy machine relays RTP traffic, using RTPProxy as backend. The SDP in INVITEs etc. is rewritten to point to the RTPProxy instance.
    There is no interaction between the balancer and the media_proxy yet, nor NAT is supported.

---
+++ provisioning/media_proxy/bootstrap.sh
@@ -0,0 +1,11 @@
+# Install facilities
+apt install --yes --force-yes kamailio*
+apt install rtpproxy
+
+# Import virtual root
+cp -rv /vroot/* /
+
+systemctl stop rtpproxy
+pkill rtpproxy
+rtpproxy -F -u vagrant -s udp:127.0.0.1:7722 -l $1
+systemctl restart kamailio
